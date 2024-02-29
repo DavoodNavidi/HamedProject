f88538528b5194393a240e76a1f4b66f5fbcfca0
Davood.Navidi <davood.dn1367@gmail.com>
2024-02-29 11:44:44 +0330

    CreateView

---
+++ Hamed.Web.UI/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hamed.Web.UI.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult CreateOrder()
+        {
+            return View();
+        }
+        public IActionResult OrderList()
+        {
+            return View();
+        }
+        public IActionResult OrderTracking()
+        {
+            return View();
+        }
+    }
+}
---
+++ Hamed.Web.UI/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hamed.Web.UI.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult UserList()
+        {
+            return View();
+        }
+        public IActionResult ConfirmUsers()
+        {
+            return View();
+        }
+    }
+}
