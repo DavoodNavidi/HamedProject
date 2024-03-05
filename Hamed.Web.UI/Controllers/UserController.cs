7a9dd09d0a1a37e75f7772b6c4f3d97d57517d5d
Davood.Navidi <davood.dn1367@gmail.com>
2024-03-05 09:28:12 +0330

    somechange

--- Hamed.Web.UI/Controllers/UserController.cs
+++ Hamed.Web.UI/Controllers/UserController.cs
@@ -33,6 +33,50 @@ namespace Hamed.Web.UI.Controllers
         }
         public IActionResult ConfirmUsers()
         {
+            var users = _userManager.Users
+                .Where(a=>!a.IsApproved)
+                .Take(50)
+                .Select(a => new AppUser
+                {
+                    UserName = a.UserName,
+                    PhoneNumber = a.PhoneNumber,
+                    StatusTitle = a.StatusTitle,
+                    Id=a.Id
+                })
+                .ToList<AppUser>();
+            return View(users);
+
+        }
+        public async Task<IActionResult> ConfirmCurrentUser(string Id)
+        {
+            var user = await _userManager.FindByIdAsync(Id);
+            if (user != null)
+            {
+                // Update user data in the database
+                user.IsApproved = true;
+                user.StatusTitle = "تایید شده";
+                var result = await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    // Handle success (e.g., redirect to a success page)
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    // Handle errors (e.g., display error messages)
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            else
+            {
+                // User not found
+                return NotFound();
+            }
+
+            // Handle other scenarios (e.g., invalid input)
             return View();
         }
         public IActionResult CreateUser()
@@ -47,7 +91,10 @@ namespace Hamed.Web.UI.Controllers
                 AppUser appUser = new AppUser
                 {
                     UserName = user.Username,
-                    PhoneNumber = user.PhoneNumber
+                    PhoneNumber = user.PhoneNumber,
+                    IsApproved=false,
+                    IsBlocked=false,
+                    StatusTitle="تایید نشده"
                 };
                 var result = _userManager.CreateAsync(appUser, user.Password).Result;
                 if (result.Succeeded)
@@ -80,6 +127,11 @@ namespace Hamed.Web.UI.Controllers
             }
             // Process the user data (e.g., save to database)
             // Redirect to another page or return a success message
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("CreateUser", "User");  // Redirect to home page after logout
         }
 
 
