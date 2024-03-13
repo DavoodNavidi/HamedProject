1e827df7088d892122b1fa601312223018628dc0
Davood.Navidi <davood.dn1367@gmail.com>
2024-03-13 15:34:53 +0330

    add role and RTL

---
+++ Hamed.Web.UI/Controllers/RoleController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hamed.Web.UI.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+        public IActionResult RoleList()
+        {
+            var roles = roleManager.Roles.ToList();
+            return View(roles);
+        }
+        public  IActionResult CreateRole(string RoleName)
+        {
+            IdentityRole identityRole = new IdentityRole
+            {
+                Name = RoleName
+            };
+            var result =  roleManager.CreateAsync(identityRole).Result;
+            
+                return RedirectToAction("RoleList");
+           
+        }
+    }
+}
